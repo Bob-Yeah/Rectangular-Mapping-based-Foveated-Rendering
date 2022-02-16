352c5402a01c8f0cdbf199156456f2c0da37f7ad
BobYeah <635596704@qq.com>
2022-02-17 01:33:51 +0800

    Readme Update

--- README.md
+++ README.md
@@ -10,38 +10,38 @@ Unity demo. Tested on Unity 2020.3.8f1
 The comparison between 
 Kernel Foveated Rendering (KFR), 
 rectangular-mapping foveated rendering (RFR),
-and the full-resolution is shown in the figure below. 
+and the full-resolution is shown in the figure below. (No denosing blur applied.)
 
-RFR (Our proposed rectangular-mapping foveated rendering, with sigma = 1.6)
+RFR (Our proposed rectangular-mapping foveated rendering, with sigma = 2.6)
 
-<img src="https://github.com/xmeng525/RecMapFoveatedRendering/blob/main/Figures/fireroom/RFR_sigma_1.6alpha_17.png" alt="" width="640" height="360" />
+<img src="https://github.com/Bob-Yeah/Rectangular-Mapping-based-Foveated-Rendering/blob/main/Sources/fireroom/RFR_sigma_2.6.png" alt="" width="360" height="400" />
 
-KFR (with sigma = 1.6)
+KFR (with sigma = 2.6)
 
-<img src="https://github.com/xmeng525/RecMapFoveatedRendering/blob/main/Figures/fireroom/KFR_sigma_1.6alpha_4.png" alt="" width="640" height="360" />
+<img src="https://github.com/Bob-Yeah/Rectangular-Mapping-based-Foveated-Rendering/blob/main/Sources/fireroom/KFR_sigma_2.6_alpha_4.png" alt="" width="360" height="400" />
 
 Full-resolution Rendering
 
-<img src="https://github.com/xmeng525/RecMapFoveatedRendering/blob/main/Figures/fireroom/Original.png" alt="" width="640" height="360" />
+<img src="https://github.com/Bob-Yeah/Rectangular-Mapping-based-Foveated-Rendering/blob/main/Sources/fireroom/Original.png" alt="" width="360" height="400" />
 
-## Comparison with KFR (Checkboard Example)
+## Comparison with KFR (Artificial Pattern Example)
 
 The comparison between 
 Kernel Foveated Rendering (KFR), 
 rectangular-mapping foveated rendering (RFR),
 and the full-resolution is shown in the figure below. 
 
-RFR (Our proposed rectangular-mapping foveated rendering, with sigma = 1.6)
+RFR (Our proposed rectangular-mapping foveated rendering, with sigma = 2.6)
 
-<img src="https://github.com/xmeng525/RecMapFoveatedRendering/blob/main/Figures/checkboard/RFR_sigma_1.6alpha_17.png" alt="" width="640" height="360" />
+<img src="https://github.com/Bob-Yeah/Rectangular-Mapping-based-Foveated-Rendering/blob/main/Sources/Patterns/RFR_sigma_2.6.png" alt="" width="360" height="400" />
 
-KFR (with sigma = 1.6)
+KFR (with sigma = 2.6)
 
-<img src="https://github.com/xmeng525/RecMapFoveatedRendering/blob/main/Figures/checkboard/KFR_sigma_1.6alpha_4.png" alt="" width="640" height="360" />
+<img src="https://github.com/Bob-Yeah/Rectangular-Mapping-based-Foveated-Rendering/blob/main/Sources/Patterns/KFR_sigma_2.6_alpha_4.png" alt="" width="360" height="400" />
 
 Full-resolution Rendering
 
-<img src="https://github.com/xmeng525/RecMapFoveatedRendering/blob/main/Figures/checkboard/Original.png" alt="" width="640" height="360" />
+<img src="https://github.com/Bob-Yeah/Rectangular-Mapping-based-Foveated-Rendering/blob/main/Sources/Patterns/Original.png" alt="" width="360" height="400" />
 
 ## Install
 Clone this repo and open the RMFR folder using Unity 2020.3.8f1.
@@ -50,21 +50,19 @@ Or Download the "RMFR.unitypackage" and import the package to Unity 2020.3.8f1.
 
 
 ## Parameter Control
-You may control the parameters within the unity inspector:
+You may control the parameters within Keys
 
-<img src="https://github.com/Bob-Yeah/Rectangular-Mapping-based-Foveated-Rendering/tree/main/Sources/Control.PNG" alt="" width="383" height="321" />
+"Sigma": control the sigma: "Q" sigma- , "E" sigma+ ;
 
-"Sigma": control the sigma before you clicking "Play" Button of Unity Editor to modify the scale factor;
+"Fx": control the parameter along x-axis: "Left" fx- , "Right" fx+;
 
-"Fx": control the parameter along x-axis (you can modify it realtime);
+"Fy": control the parameter along y-axis: "Up" fy+, "Down" fy-;
 
-"Fy": control the parameter along y-axis (you can modify it realtime);
+"Eye X": control the x-component of eye gaze position: "A" eyeX-, "D" eyeX+;
 
-"Eye X": control the x-component of eye gaze position (you can modify it realtime);
+"Eye Y": control the y-component of eye gaze position: "W" eyeY+, "S" eyeY-;
 
-"Eye Y": control the y-component of eye gaze position (you can modify it realtime);
-
-If you want to change the display resolution, the Width, Height in the inspector, the corresponding settings of the "Game Window", and the size of the rendertextures TexturePass0, TexturePass1, TextureDenoise should ALL be modified.
+If you want to change the display resolution, the corresponding settings of the "Game Window", and the size of the rendertextures TexturePass0 should ALL be modified manually.
 
 Please note that this project only shows the effect of rectangular-mapping foveated rendering. 
 The speedup cannot be shown in this project because the rendering task here is not complex.
--- RMFR/Assets/RFR_SingleEye.cs
+++ RMFR/Assets/RFR_SingleEye.cs
@@ -5,18 +5,14 @@ using UnityEngine;
 public class RFR_SingleEye : MonoBehaviour
 {
     public RenderTexture TexturePass0;
-    //public RenderTexture TexturePass1 = null;
     RenderTexture TexturePass1 = null;
-
-    public RenderTexture TexturePass2;
-    public RenderTexture TextureDenoise;
+    RenderTexture TexturePass2;
+    RenderTexture TextureDenoise;
     public Material Pass1Material;
     public Material Pass2Material;
     public Material DenoiseMaterial;
 
-    public int width = 1440;
-    public int height = 1600;
-    public float sigma;
+    public float sigma = 0.3f;
     [Range(0.01f, 0.99f)]
     public float fx;
     [Range(0.01f, 0.99f)]
@@ -28,21 +24,39 @@ public class RFR_SingleEye : MonoBehaviour
     int iApplyRFRMap1;
     int iApplyRFRMap2;
 
+    public string savePath;
+
+    float sigma0;
+
+    bool b_save = false;
+
     // Start is called before the first frame update
     void Start()
     {
-        width = 1440;
-        height = 1600;
+        sigma0 = sigma;
         iApplyRFRMap1 = 1;
         iApplyRFRMap2 = 1;
-        TexturePass1 = new RenderTexture(Mathf.RoundToInt(width / sigma), Mathf.RoundToInt(height / sigma), 24, RenderTextureFormat.Default);
+
+        TexturePass1 = new RenderTexture(Mathf.RoundToInt(Screen.width / sigma), Mathf.RoundToInt(Screen.height / sigma), 24, RenderTextureFormat.Default);
         TexturePass1.Create();
+
+        TexturePass2 = new RenderTexture(Mathf.RoundToInt(Screen.width), Mathf.RoundToInt(Screen.height), 24, RenderTextureFormat.Default);
+        TexturePass2.Create();
+
+        TextureDenoise = new RenderTexture(Mathf.RoundToInt(Screen.width), Mathf.RoundToInt(Screen.height), 24, RenderTextureFormat.Default);
+        TextureDenoise.Create();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        keyControl();
+        saveImages();
+        if (sigma0 != sigma)
+        {
+            updateTextureSize();
+            sigma = sigma0;
+        }
     }
 
     private void OnRenderImage(RenderTexture src, RenderTexture dst)
@@ -77,13 +91,107 @@ public class RFR_SingleEye : MonoBehaviour
 
     void Pass3DenoiseL()
     {
-        DenoiseMaterial.SetFloat("_iResolutionX", width);
-        DenoiseMaterial.SetFloat("_iResolutionY", height);
+        DenoiseMaterial.SetFloat("_iResolutionX", Screen.width);
+        DenoiseMaterial.SetFloat("_iResolutionY", Screen.height);
         DenoiseMaterial.SetFloat("_eyeX", eyeX);
         DenoiseMaterial.SetFloat("_eyeY", eyeY);
         DenoiseMaterial.SetTexture("_Pass2Tex", TexturePass2);
         Graphics.Blit(null, TextureDenoise, DenoiseMaterial);
     }
 
+    void keyControl()
+    {
+        if (Input.GetKeyDown(KeyCode.Q))
+            sigma0 = sigma0 <= 1.2f ? 1.2f : sigma0 - 0.2f;
+        if (Input.GetKeyDown(KeyCode.E))
+            sigma0 = sigma0 >= 2.8f ? 2.8f : sigma0 + 0.2f;
+        
+        if (Input.GetKeyDown(KeyCode.A))
+            eyeX = eyeX <= 0.05f ? 0.05f : eyeX - 0.05f;
+        if (Input.GetKeyDown(KeyCode.D))
+            eyeX = eyeX >= 0.95f ? 0.95f : eyeX + 0.05f;
 
+        if (Input.GetKeyDown(KeyCode.W))
+            eyeY = eyeY >= 0.95f ? 0.95f : eyeY + 0.05f;
+        if (Input.GetKeyDown(KeyCode.S))
+            eyeY = eyeY <= 0.05f ? 0.05f : eyeY - 0.05f;
+
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+            fx = fx <= 0.15f ? 0.1f : fx - 0.1f;
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+            fx = fx >= 0.85f ? 0.9f : fx + 0.1f;
+
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+            fy = fy >= 0.85f ? 0.9f : fy + 0.1f;
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+            fy = fy <= 0.15f ? 0.1f : fy - 0.1f;
+
+        if (Input.GetKeyDown(KeyCode.F9))
+            b_save = !b_save;
+    }
+
+    void updateTextureSize()
+    {
+        RenderTexture tempTexture = new RenderTexture(Mathf.RoundToInt(Screen.width / sigma0), Mathf.RoundToInt(Screen.height / sigma0), 24, RenderTextureFormat.Default);
+        tempTexture.Create();
+
+        TexturePass1.Release();
+        TexturePass1 = tempTexture;
+    }
+
+    void saveImages()
+    {
+        if (b_save)
+        {
+            Debug.Log("eyeXL:" + eyeX.ToString() + "\teyeYL:" + eyeY.ToString() + 
+            "sigma:" + sigma + "\n" + "fx:" + fx.ToString() + "\tfy:" + fy.ToString());
+
+            SaveToFile(TexturePass0, savePath + "/original_left_n.png");
+
+            SaveToFile(TexturePass1, savePath +"/sigma_" +
+               sigma.ToString() + "_fx_" + fx.ToString() + "_fy_" + fy.ToString() +
+               "_eX_" + eyeX.ToString() + "_eY_" + eyeY.ToString() + "_p1.png");
+
+            SaveToFile(TexturePass2, savePath + "/sigma_" +
+               sigma.ToString() + "_fx_" + fx.ToString() + "_fy_" + fy.ToString() +
+               "_eX_" + eyeX.ToString() + "_eY_" + eyeY.ToString() + "_p2.png");
+
+            SaveToFile(TextureDenoise, savePath + "/sigma_" +
+               sigma.ToString() + "_fx_" + fx.ToString() + "_fy_" + fy.ToString() +
+               "_eX_" + eyeX.ToString() + "_eY_" + eyeY.ToString() + "_dn.png");
+
+            b_save = false;
+        }
+
+    }
+
+    public void SaveToFile(RenderTexture renderTexture, string name)
+    {
+        RenderTexture currentActiveRT = RenderTexture.active;
+        RenderTexture.active = renderTexture;
+        Texture2D tex = new Texture2D(renderTexture.width, renderTexture.height);
+        tex.ReadPixels(new Rect(0, 0, tex.width, tex.height), 0, 0);
+        var bytes = tex.EncodeToPNG();
+        System.IO.File.WriteAllBytes(name, bytes);
+        UnityEngine.Object.Destroy(tex);
+        RenderTexture.active = currentActiveRT;
+    }
+
+    void DispText(int idx, float variable, string name)
+    {
+        GUIStyle guiStyle = new GUIStyle();
+        guiStyle.fontSize = 50;
+        string text = string.Format(name + " = {0}", variable);
+        GUI.Label(new Rect(0, idx * 50, Screen.width, Screen.height), text, guiStyle);
+    }
+
+    void OnGUI()
+    {
+        int idx = 0;
+        DispText(idx++, sigma, "sigma");
+        DispText(idx++, fx, "fx");
+        DispText(idx++, fy, "fy");
+        DispText(idx++, eyeX, "eyeX");
+        DispText(idx++, eyeY, "eyeY");
+    }
 }
